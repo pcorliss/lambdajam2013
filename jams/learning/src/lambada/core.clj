05365d9e77a514df4767f62d8dd34e199c40fb55
Philip Corliss <pcorliss@groupon.com>
2013-07-09 14:13:00 -0500

    compare grid

--- jams/learning/src/lambada/core.clj
+++ jams/learning/src/lambada/core.clj
@@ -17,6 +17,16 @@
     (for [element line]
       (Long/parseLong element))))
 
+(defn diff-squared
+  "Finds the difference between numbers and squares it"
+  [n m]
+  (* (- n m) (- n m)))
+
+(defn compare-grid
+  "Compares two number grids and returns the RMS"
+  [grid1 grid2]
+  (. Math sqrt (/ (apply + (map diff-squared grid1 grid2)) 784)))
+
 (defn foo
   "I don't do a whole lot."
   [x]
