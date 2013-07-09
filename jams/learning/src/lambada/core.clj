328c8570dd3cfbe11953db1e5fe061720af536d6
Philip Corliss <pcorliss@groupon.com>
2013-07-09 13:41:47 -0500

    Loading data

--- jams/learning/src/lambada/core.clj
+++ jams/learning/src/lambada/core.clj
@@ -1,5 +1,15 @@
 (ns lambada.core)
 
+(require '[clojure.data.csv :as csv]
+         '[clojure.java.io :as io])
+
+(defn read-data
+  "Read in data"
+  [filename]
+  (with-open [in-file (io/reader filename)]
+    (doall
+      (csv/read-csv in-file :separator \|))))
+
 (defn foo
   "I don't do a whole lot."
   [x]
